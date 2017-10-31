5d271a5edb6b610fec38b577892b75c19c0c66c1
depthgr8 <depthgr8@gmail.com>
2017-10-31 22:33:07 +0530

    Relations for sample voxels

--- comparator.R
+++ comparator.R
@@ -1,8 +1,14 @@
+# R script that compare voxel intensities
 # Date: October 30, 2017
 # Author: Deepak Sharma
  
 # install.packages("combinat")
 library(combinat)
-colss <- c("V1","V2","V3","V4")
-rels <- combn2(colss)
-cat(nrow(rels),"relations generated for",length(colss), "variables")
+orig_dat <- read.table("data.txt")
+sam_dat <- orig_dat[1:10,1:20]
+View(sam_dat)
+cols <- colnames(sam_dat)
+rels <- combn2(cols)
+cat(nrow(rels),"relations generated for",length(cols), "variables")
+combn2(cols)
+
