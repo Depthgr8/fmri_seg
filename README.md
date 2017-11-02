8324a9b2df44997cfa5092f717e1829dae5c1878
depthgr8 <depthgr8@gmail.com>
2017-11-02 23:29:21 +0530

    Extract only neighbor voxels

--- README.md
+++ README.md
@@ -1,4 +1,16 @@
 # fmri_seg
-Segmentation of fMRI data
 
-- Detecting tasks in fMRI data using neural networks.
+Segmentation of f-MRI data using AI inspired algorithms.
+
+# Todo
+
+-[x] Read functional MRI data in `.CSV` files
+-[x] For all voxels, get cartesian coordinate informations
+-[x] For all voxels, generate all possible neighbor voxel relations
+-[x] Define functions to check neighborhood in cartesian coordinates
+-[x] Extract neighbor voxels
+-[ ] Calculate processing time
+-[ ] Detecting tasks in fMRI data using neural networks.
+
+
+
--- comparator.R
+++ comparator.R
@@ -1,23 +1,39 @@
 # Documenation ------------------------------------------------------------
 
-# R script that compare voxel intensities
+# R script to compare voxel intensities
 # Date: October 30, 2017
 # Author: Deepak Sharma
- 
+
+# Basic clean up ----------------------------------------------------------
+
+cat('\f')     # Clear console
+rm(list=ls()) # Clear all variables
+
+# Start stopwatch ---------------------------------------------------------
+
+start_time <- proc.time()
+
 # Read data ---------------------------------------------------------------
-orig <- read.csv(file = "fmri.csv",sep = ",",header = F)
-samp <- orig[1:5,1:10]    # Take short sample of original for fast testing
+
+orig <- read.csv(file = "fmri.csv", sep = ",", header = F)
+# orig <- orig[1:5,1:10]    # Take short sample of original for fast testing
 
 # Functions defined -------------------------------------------------------
+
+# Get voxel's cartesian coordinates ---------------------------------------
+
 vx <- function(x){
-  samp[1,x]
+  orig[1,x]
 }
 vy <- function(y){
-  samp[2,y]
+  orig[2,y]
 }
 vz <- function(z){
-  samp[3,z]
+  orig[3,z]
 }
+
+# Check neighborhood in cartesian coordinates -----------------------------
+
 xneighbor <- function(xv){
   if(xv==0 || xv==-1 || xv==1)
     return(T)
@@ -39,24 +55,30 @@ zneighbor <- function(zv){
 
 # Generate relations ------------------------------------------------------
 
-voxels <- 1:ncol(samp)
+voxels <- 1:ncol(orig)
 rels <- t(combn(voxels,2))
-# image(rels)
 
 # Extract neighbors -------------------------------------------------------
 
-cat(dim(rels))
-dim(orig)
-View(samp)
-
-neighbors <- matrix(nrow = dim(rels)[1], ncol = dim(rels)[2],data = 0)
-neighbors
-
+neighbors <- matrix(nrow = dim(rels)[1], ncol = dim(rels)[2],data = NA)
 for(i in 1:dim(rels)[1]){
-  if(xneighbor(vx(rels[i,1]) - vx(rels[i,2])) || yneighbor(vy(rels[i,1]) - vy(rels[i,2])) || zneighbor(vz(rels[i,1]) - vz(rels[i,2]))){
+  if(xneighbor(vx(rels[i,1]) - vx(rels[i,2])) && yneighbor(vy(rels[i,1]) - vy(rels[i,2])) && zneighbor(vz(rels[i,1]) - vz(rels[i,2]))){
     neighbors[i,1] <- rels[i,1]
     neighbors[i,2] <- rels[i,2]   
   }
   
 }
-neighbors
+
+# Data summary ------------------------------------------------------------
+
+cat("Total relations = ",length(neighbors),"\n")
+true_nhood <- cat("Neighborhood relations = ",sum(!is.na(neighbors)))
+
+# Data visualization ------------------------------------------------------
+
+image(neighbors)
+
+# Stop stopwatch ----------------------------------------------------------
+
+time_taken <- proc.time() - start_time 
+cat("Time taken = ",unname(time_taken[1]),"ms\n")
